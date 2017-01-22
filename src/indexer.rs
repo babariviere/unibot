f2cc5cdc678153902bd1ffb946e58ab687c68147
babariviere <babariviere@protonmail.com>
2017-01-22 13:37:22 +0100

    Starting indexer

--- src/indexer.rs
+++ src/indexer.rs
@@ -1,3 +1,5 @@
+use errors::*;
+use hyper::client::IntoUrl;
 use site::Site;
 
 #[derive(Debug, Default)]
@@ -11,4 +13,43 @@ impl Indexer {
     }
 
     // TODO - index site - is indexed - get all main urls - get url - get sub url
+
+    /// Add a url to indexer
+    ///
+    /// If url is not indexed, then it will create a new site with this url
+    pub fn add_url<U: IntoUrl>(&mut self, url: U) -> Result<()> {
+        let url = url.into_url()?;
+        for site in &mut self.sites {
+            if site.contains_url(url.clone()) {
+                return Ok(());
+            }
+            if site.is_same_host(url.clone()) {
+                site.add_sub_url(url);
+                return Ok(());
+            }
+        }
+
+        self.sites.push(Site::new(url)?);
+        Ok(())
+    }
+
+    /// Return all sites that indexer provide
+    pub fn get_sites(&self) -> &Vec<Site> {
+        &self.sites
+    }
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use super::Indexer;
+
+    #[test]
+    fn add_url() {
+        let mut indexer = Indexer::new();
+        indexer.add_url("http://example.com").unwrap();
+        indexer.add_url("http://example.com/hello").unwrap();
+        indexer.add_url("http://example.com/hello/world").unwrap();
+        indexer.add_url("http://google.com").unwrap();
+        assert_eq!(indexer.get_sites().len(), 2);
+    }
 }
